203a6171621cf846a8b552ae8b711c94054cd8ca
root <root@localhost.localdomain>
2017-08-27 02:42:54 -0700

    [+] Client can now communicate with server endlessly [+] Added Exceptions to the client( Broken Pipe and Connection Reset) [+] Added some comments [+] Organized both programs a bit more (still needs work) [-] Need to figure out how multiple users could be implemented

--- Python-Messenger/client.py
+++ Python-Messenger/client.py
@@ -3,30 +3,55 @@
 import socket
 import time
 import sys
+import multiprocessing
+
+#Client for the messaging program
+
+def recieveMessages(client):
+	data = client.recv(1024).decode() #Decodes incoming binary data
+	if not data:
+		pass #If the recieved data is empty the function just passes
+	else:
+		print(data)
 
 def sendMessages(username, client):
-	while 1:
-		message = input(username + " >> ")
-		if( message == "/disconnect"):
-			sys.exit()
-		client.send((username+ " >> "+message).encode())
-	
-def main():
+#The client is set to send the first message, and the server recieve
+	message = input(username + " >> ")
+	if( message == "/dis"):
+		client.send((username + " >> Disconnecting...").encode())
+		client.shutdown(socket.SHUT_RDWR)
+		client.close()
+		print("Connection is now closed.")
+		sys.exit()
+	else:
+		client.send((username + " >> " + message).encode()) # This encodes the string into binary data to be transmitted
+def connectToServer(client):
 	while True:
-		client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		print("[+] Connecting...")
 		try:
-                    client.connect(('192.168.1.10', 6677))
-                    break
-
+			client.connect(('192.168.1.10', 6677))
+			break
 		except: 
 			print("Connection Failed, Retrying...")
 			time.sleep(3)
-	
-	print("[+] Connected")
-	print(client.getpeername())
+	print("Connected to: " + str(client.getpeername()))
+
+def main():
+#This loop is going to run until the client finds the server it wants to connect to
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	connectToServer(client)
 	username = input("Enter your Username: ")
-	sendMessages(username, client)
-					
+	print("Type '/dis' to end connection")
+	while True:
+		try:
+			sendMessages(username, client)
+			recieveMessages(client) #After the user has send their message, they will now listen
+		except ConnectionResetError:
+			print("[-] Connection has been lost by other user")
+			main()
+		except BrokenPipeError:
+			print("[-] Message was not sent, Broken Pipe")
+			main()
+
 if __name__ == "__main__":
 	main()
--- Python-Messenger/server.py
+++ Python-Messenger/server.py
@@ -3,37 +3,51 @@
 import socket
 import time
 import sys
-import multiprocessing as mp
 
-mp.allow_connection_pickling()
+#Server program meant to communicate with client
+#Goal: communicate with sever clients
+def terminateSession(conn, server):
+	server.shutdown(socket.SHUT_RDWR)
+	server.close()
+	print("Connection is now closed")
+	sys.exit()
 
-def recieveMessage(conn):
-	while True:
+def sendMessages(conn, server, username):
+	message = input(username + " >> ")
+	if(message == "/dis"):
+		conn.send((username + " >> Disconnecting...").encode())
+		terminateSession(conn, server)
+	else:
+		conn.send((username + " >> " + message).encode())
+
+
+def recieveMessages(conn):
 		data = conn.recv(1024).decode()
-		if(data !=''):
-			print(data)		
-
+		if not data:
+			pass
+		else:
+			print(data)
 
 def main():
 	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
 
 	try:
+		print("[+] Binding...")
 		server.bind(('', 6677))
+		print("[+] Bind was a success!")
 	except socket.error as msg:
 		print("Bind was a failure, now quiting...")
 		sys.exit()
 
-	server.listen(2)
+	server.listen(5)
+	username = input("Please enter username: ")
 	print("[+] Server is Listening")
-	while True:	
-		conn, addr = server.accept()
-		print("Connected with", addr)
-		clientHandling = mp.Process(target = recieveMessage, args=(conn,))
-		clientHandling.start()
-
-	server.shutdown(socket.SHUT_RDWR)
-	server.close()
+	conn, addr = server.accept()
+	print("Connected with", addr)
+	while True:
+		recieveMessages(conn)
+		sendMessages(conn, server, username)
 
 if __name__ == "__main__":
 	main()
